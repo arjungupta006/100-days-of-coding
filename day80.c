779844ae215e99a55d28e0e8f1fd99d0a32cb531
Arjun Gupta <arjungupta34006@gmail.com>
2025-11-30 01:58:10 +0530

    Day80.c

--- day80.c
+++ day80.c
@@ -11,8 +11,45 @@ Name: Ravi | Roll: 102 | Marks: 92
 */
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    FILE *fp;
+    int n;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    fp = fopen("students.txt", "w");
+    if (fp == NULL) return 0;
+
+    for (int i = 0; i < n; i++) {
+        char name[50];
+        int roll, marks;
+
+        printf("Student %d Name: ", i + 1);
+        scanf("%s", name);
+
+        printf("Student %d Roll: ", i + 1);
+        scanf("%d", &roll);
+
+        printf("Student %d Marks: ", i + 1);
+        scanf("%d", &marks);
+
+        fprintf(fp, "%s %d %d\n", name, roll, marks);
+    }
+
+    fclose(fp);
+
+    fp = fopen("students.txt", "r");
+    if (fp == NULL) return 0;
+
+    char name[50];
+    int roll, marks;
+
+    while (fscanf(fp, "%s %d %d", name, &roll, &marks) != EOF) {
+        printf("Name: %s | Roll: %d | Marks: %d\n", name, roll, marks);
+    }
+
+    fclose(fp);
 
     return 0;
 }
